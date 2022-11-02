c53f8166d1e4c21b320f692eee6e021cb3d0370f
VladyslavMarkov <114297797+VladyslavMarkov@users.noreply.github.com>
2022-11-02 20:40:46 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,32 +3,33 @@
 #include <time.h>// чтобы использовать функцию time()
 #include "lib.h"
 
-
-
 int main(int argc, char *argv[])
 {
+    //Змінна t - для функції lab06 і для запису аргументів через командний рядок
+    //Змінна x та y - діапазон у якому потрібно шукати прості чила у функції lab05
     long int t = 5000;
-    int x, y, res1, res2, res[9999];
+    int x,y;
     char *c;
-       
+     
+    //Примушує функцію rand() завжди генерувати рандомне число  
     srand((unsigned int)time(NULL));
     x = rand() % 100; 
     y = rand() % (x-1);    
-    lab05(x, y); 
-      
-     
+    lab05(x, y);   
+    
+    //Перевірка чи вводили ми якісь аргументи в командному рядку
     if(argc > 1)
     {
       t = strtol(argv[1], &c, 10);
       if(t > 1000 && t < 9999)
       {
-      lab06(t);
+        lab06(t);
       }
-    }
-    else
-    {    
-      lab06(t);     
-    }
+
+    }else
+      {      
+        lab06(t);      
+      }
     
     return 0;
 }
